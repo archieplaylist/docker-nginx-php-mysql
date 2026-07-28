16b183bb49994afba85d610ab7f7fed201062743
archieplaylist <mario.tani25@gmail.com>
2026-07-28 08:22:29 +0700

    Update .gitignore and docker-compose.yml for volume configurations; refactor index.php for improved layout and navigation links

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,77 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
+<head>
+  <title>List Web PK</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    header {
+      text-align: center;
+      margin-bottom: 20px;
+    }
 
-?><!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
-    </head>
-    <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
-    </body>
+    ul {
+      list-style: none;
+      padding: 0;
+    }
+
+    li {
+      margin: 10px;
+    }
+
+    a {
+      font-size: 20px;
+      font-weight: bold;
+      color: blue;
+      text-decoration: none;
+      transition: color 0.5s;
+    }
+
+    a:hover {
+      color: green;
+    }
+
+    main {
+      text-align: center;
+      margin: auto;
+      width: 50%;
+      border: 1px solid black;
+      padding: 10px;
+    }
+
+    footer {
+      text-align: center;
+      font-style: italic;
+      margin-top: 20px;
+    }
+  </style>
+</head>
+<header>
+  <h1>List Web PK</h1>
+</header>
+<body>
+  <main>
+    <ul>
+      <!-- <br/><br/> -->
+      <li><a href="auth/public/login/" target="_blank">AUTH</a></li>
+      <li><a href="inv_dev/public/login" target="_blank">INVENTARIS - (LAMA)</a></li>
+      <li><a href="inv/public/login" target="_blank">INVENTARIS - (BARU)</a></li>
+      <li><a href="spo_suhu/public/" target="_blank">SPO SUHU</a></li>
+      <li><a href="suhu_temp/public/" target="_blank">CCTV & SUHU MON</a></li>
+      <li><a href="suhu_gpf/public/" target="_blank">SUHU MON GPF</a></li>
+      <li><a href="suhu_far/public/" target="_blank">SUHU MON FARMASI</a></li>
+      <li><a href="suhu_dap/public/" target="_blank">SUHU MON DAPUR</a></li>
+      <li><a href="rad/public/login/" target="_blank">List Data Radiologi</a></li>
+      <li><a href="http://home.rkzsby.local/komputer" target="_blank">List Data Komputer Local </a></li>
+      <li><a href="http://home.rkzsby.local/telpon/" target="_blank">List Data Telepon Local </a></li>
+      <li><a href="barcode/index.html" target="_blank">BARCODE GENERATOR </a></li>
+      <li><a href="firefox" target="_blank">FIREFOX DOWNLOAD</a></li>
+    </ul>
+  </main>
+  <footer>
+    <p>&copy; 2023 List Web PK. All rights reserved.</p>
+  </footer>
+</body>
+
 </html>
